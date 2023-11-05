191d6e3095a4aa7dc0827b4dc1de203586f6de61
AnastasiiaLevkina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2023-11-05 19:04:10 +0000

    page design

--- dom.js
+++ dom.js
@@ -3,3 +3,8 @@ for (i = 0; i < 5; i++) {
     randomNumber.push(Math.floor(Math.random()*10));
   }
 console.log(randomNumber);
+
+function verifyNumber(){
+  let guessNumber = document.getElementsByTagName("form");
+  console.log(guessNumber)
+}
