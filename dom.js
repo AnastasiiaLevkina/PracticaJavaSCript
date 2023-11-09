1accdacb494afc3eda129c2204a8d48a1e09c901
AnastasiiaLevkina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2023-11-09 21:31:56 +0000

    entregaFinal

--- dom.js
+++ dom.js
@@ -3,24 +3,29 @@ for (i = 0; i < 5; i++) {
   randomNumber += (Math.floor(Math.random()*10)).toString();
 }
 
+console.log(randomNumber);
+
 let correct;
 let almostCorrect;
 let guessed;
 
 function coincidences(guessed, guessNum, randomNum){
   let randomNum2 = randomNum;
-  for (k=0; k<2; k++){
-     for (i=0; i<5; i++){
-       for (j = 0; j<5; j++){
-         if (guessNum[i]==randomNum2[j] && j==i ) {
-          guessed[i] = 2;
-          randomNum2 = randomNum2.replace(randomNum2[i], 'a');
-        }
-         else if (randomNum2.includes(guessNum[i])) guessed[i] = 1;
-         else if (guessed[i]<2) guessed[i] = 0;
-        }
-     }
+  for (j = 0; j<2; j++){
+    for (i = 0; i<5; i++){
+      if (guessNum[i]==randomNum[i]) {
+        guessed[i] = 2;
+        randomNum2 = randomNum2.replace(randomNum2[i], 'a');
+      }
+      else if (randomNum2.includes(guessNum[i])) {
+        guessed[i] = 1;
+        if (j>0) randomNum2 = randomNum2.replace(guessNum[i], 'b');
+      }
+      else if (guessed[i]<1 || !randomNum2.includes(guessNum[i])) guessed[i] = 0;
+      console.log(randomNum2);
+    }
   }
+  console.log(guessed);
   return guessed
 }
 
@@ -32,16 +37,16 @@ function verifyNumber(){
   let errorMessage = document.getElementsByClassName("error");
   
   if (!Number.isInteger(Number(guessNumber))){
-    errorMessage[0].innerHTML = "Esto no es un número!";
+    errorMessage[0].innerHTML = "¡Esto no es un número!";
   }
 
   else if (guessNumber>99999){
-      errorMessage[0].innerHTML = "Este es un número demasiado grande!";
+      errorMessage[0].innerHTML = "¡Este es un número demasiado grande!";
   }
 
   else if (guessNumber != randomNumber) {
     // Error message
-    errorMessage[0].innerHTML = "Número erróneo, intenta otra vez!";
+    errorMessage[0].innerHTML = "¡Número erróneo, intente otra vez!";
     guessNumber = guessNumber.toString();
 
     // Remove null values
@@ -82,11 +87,9 @@ function verifyNumber(){
     }
 
   else {
-    errorMessage[0].innerHTML = "Has adivinado el número!";
+    errorMessage[0].innerHTML = "¡Ha adivinado el número!";
     let secretNumber = document.getElementById("random");
     let secretFigure = secretNumber.getElementsByClassName("numberSquare");
-    for (i = 0; i<5; i++){
-      secretFigure[i].innerHTML = randomNumber[i];
-      }
+    for (i = 0; i<5; i++) secretFigure[i].innerHTML = randomNumber[i];
   }
 }
